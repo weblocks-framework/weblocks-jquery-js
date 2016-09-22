5d6bb56efb91304fdd4811febe3a0a506eb5ec2a
Olexiy Zamkoviy <olexiy.z@gmail.com>
2016-09-22 14:12:21 +0300

    Improved library towards new tiny api for js backends

--- weblocks-jquery-js.lisp
+++ weblocks-jquery-js.lisp
@@ -5,13 +5,16 @@
 (defmethod weblocks-webapp-default-dependencies :around ((self weblocks-webapp))
   (append 
     (when (equal (weblocks:weblocks-webapp-js-backend self) :jquery)
-      (let ((base-path  (weblocks::asdf-system-directory :weblocks-jquery-js)))
-        '((:script "jquery-1.8.2" :default t)
-          (:script "jquery-seq" :default t)
-          (:script "weblocks-jquery" :default t))))
+      (weblocks-webapp-dependencies-for-backend self :jquery))
     (call-next-method)))
 
-(defmethod initialize-webapp :after ((self weblocks-webapp))
+(defmethod weblocks-webapp-dependencies-for-backend ((self weblocks-webapp) (backend (eql :jquery)))
+  (let ((base-path  (weblocks::asdf-system-directory :weblocks-jquery-js)))
+    '((:script "jquery-1.8.2" :default t)
+      (:script "jquery-seq" :default t)
+      (:script "weblocks-jquery" :default t))))
+
+(defmethod weblocks-webapp-init-dependencies-for-backend ((self weblocks-webapp) (backend (eql :jquery)))
   (weblocks-utils:require-assets 
     "https://raw.github.com/html/weblocks-assets/master/jquery/1.8.2/"
     :webapp self)
@@ -21,3 +24,7 @@
   (weblocks-utils:require-assets 
     "https://raw.github.com/html/weblocks-assets/master/weblocks-jquery/0.2.0/"
     :webapp self))
+
+(defmethod initialize-webapp :after ((self weblocks-webapp))
+  (when (equal (weblocks-webapp-js-backend self) :jquery)
+    (weblocks-webapp-init-dependencies-for-backend self :jquery)))
