f3df7484c0947bf292433607dd1446610067768e
Alexander Artemenko <svetlyak.40wt@gmail.com>
2017-05-14 13:30:29 +0300

    Methods were fixed to work consistently when both prototype and jquery systems were loaded.
    Requires changes from pull https://github.com/skypher/weblocks/pull/52

--- weblocks-jquery-js.lisp
+++ weblocks-jquery-js.lisp
@@ -2,19 +2,19 @@
 
 (in-package #:weblocks-jquery-js)
 
-(defmethod weblocks-webapp-default-dependencies :around ((self weblocks-webapp))
-  (append 
-    (when (equal (weblocks:weblocks-webapp-js-backend self) :jquery)
-      (weblocks-webapp-dependencies-for-backend self :jquery))
-    (call-next-method)))
 
-(defmethod weblocks-webapp-dependencies-for-backend ((self weblocks-webapp) (backend (eql :jquery)))
-  (let ((base-path  (weblocks::asdf-system-directory :weblocks-jquery-js)))
-    '((:script "jquery-1.8.2" :default t)
-      (:script "jquery-seq" :default t)
-      (:script "weblocks-jquery" :default t))))
+(defmethod get-js-backend-dependencies (app (backend (eql :jquery)))
+  (list (make-instance 'weblocks:script-dependency
+                       :url "/pub/scripts/jquery-1.8.2.js")
+        (make-instance 'weblocks:script-dependency
+                       :url "/pub/scripts/jquery-seq.js")
+        (make-instance 'weblocks:script-dependency
+                       :url "/pub/scripts/weblocks-jquery.js")))
 
-(defmethod weblocks-webapp-init-dependencies-for-backend ((self weblocks-webapp) (backend (eql :jquery)))
+
+(defmethod initialize-js-backend ((self weblocks-webapp) (backend (eql :jquery)))
+  (log:debug "Initializing jquery js backend")
+
   (weblocks-utils:require-assets 
     "https://raw.github.com/html/weblocks-assets/master/jquery/1.8.2/"
     :webapp self)
@@ -25,6 +25,3 @@
     "https://raw.github.com/html/weblocks-assets/master/weblocks-jquery/0.2.0/"
     :webapp self))
 
-(defmethod initialize-webapp :after ((self weblocks-webapp))
-  (when (equal (weblocks-webapp-js-backend self) :jquery)
-    (weblocks-webapp-init-dependencies-for-backend self :jquery)))
